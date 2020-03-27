e6194e0a72e7e619d3ad8ff5d876f68973587bbf
ejohnson24 <ejohnson24@users.noreply.github.com>
2020-03-27 11:41:02 -0700

    updated logo animation again

--- js/about.js
+++ js/about.js
@@ -13,7 +13,7 @@ gsap.from( '#Moon', {
 
   
   gsap.from( '#Text', {
-    y: 10,
+    y: 30,
     x: 30,
     scaleY: 0.001,
     duration: 1,
@@ -32,10 +32,10 @@ gsap.from( '#Moon', {
   gsap.from( '#B', {
     scaleX: 0.001,
     scaleY: 0.001,
-    y: -200,
     opacity: 0,
-    duration: 0.5,
+    duration: 1,
     x: 30,
+    y: -30,
     delay: 1,
     ease: "SlowMo",
     
